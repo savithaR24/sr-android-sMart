62a0fa2f3458e9485679e03afd213be5bff0ede3
savithaR24 <157411007+savithaR24@users.noreply.github.com>
2024-04-26 13:17:38 +0530

    Added Splash Screen

--- app/src/main/java/com/kodeco/smart/MainActivity.kt
+++ app/src/main/java/com/kodeco/smart/MainActivity.kt
@@ -10,11 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.kodeco.smart.ui.theme.SMartTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
         setContent {
             SMartTheme {
                 // A surface container using the 'background' color from the theme
